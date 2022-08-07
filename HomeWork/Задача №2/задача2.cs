2e4ada97301a1043b2a46957aecc955eb00da776
Alex <alexey.kalyagin@ya.ru>
2022-08-07 21:59:07 +0300

    сделал задачу 2

---
+++ HomeWork/Задача №2/задача2.cs
@@ -0,0 +1,18 @@
+﻿// Задача №2
+
+
+Console.WriteLine("Введите число А ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
