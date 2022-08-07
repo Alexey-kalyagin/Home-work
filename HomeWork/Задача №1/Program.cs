63158b4fbdd45f43a6909c0c11948ad0e38268b4
Alex <alexey.kalyagin@ya.ru>
2022-08-07 21:12:28 +0300

    задача1

---
+++ HomeWork/Задача №1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача № 1
+
+Console.WriteLine("Введите число А ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else {
+    Console.WriteLine(numberB);
+}
