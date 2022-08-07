e6fc723ed40b44262045c6fd0d1ca24d7397a329
Alex <alexey.kalyagin@ya.ru>
2022-08-07 22:44:23 +0300

    задача3 решена

---
+++ HomeWork/Задача №3/задача3.cs
@@ -0,0 +1,33 @@
+﻿// Задача №3
+
+int A = 4;
+int B = -3;
+int C = 7;
+
+if (A % 2 == 0)
+{
+    Console.WriteLine("Число чётное ");
+    Console.WriteLine(A);
+}
+else
+{
+    Console.WriteLine("Число нечётное ");
+}
+if (B % 2 == 0)
+{
+    Console.WriteLine("Число чётное ");
+    Console.WriteLine(B);
+}
+else
+{
+    Console.WriteLine("Число нечётное ");
+}
+if (C % 2 == 0)
+{
+    Console.WriteLine("Число чётное ");
+    Console.WriteLine(C);
+}
+else
+{
+    Console.WriteLine("Число нечётное ");
+}
