cc068607629f127e0357e3f21574cd75548ab4aa
Alex <alexey.kalyagin@ya.ru>
2022-08-08 22:28:42 +0300

    задача4

---
+++ HomeWork/Задача №4/Задача4.cs
@@ -0,0 +1,19 @@
+﻿// Задача №4
+
+int N = 5;
+int index = 1;
+
+while (index < 5)
+{
+    if (index % 2 == 0)
+    {
+        Console.WriteLine("Число чётное ");
+        Console.WriteLine(index);
+    }
+    else
+    {
+    Console.WriteLine("Число нечётное ");       
+    }
+index = index + 1;
+
+}
